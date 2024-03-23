853b5c594a4d6b4346dfb0b8f28dfbf31b30a415
benyaminsoldier <daniel.benjumea0@gmail.com>
2024-03-22 20:26:56 -0600

    index.js modified with basic understanding stuff

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('my first server fucker')
+const http = require('http')
+const server = http.createServer((req,res)=>{
+    let url =  req.url
+    if(url==='/'){
+        res.write('<p>fucker</p>')
+        console.log('fucker')
+    }
+})
+server.listen(7000,()=>{
+    console.log('server is running...')
+})
